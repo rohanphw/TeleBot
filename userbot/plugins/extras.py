3f2c59ef0cbb6cf0e066446e30b66a2ac4b6c2ed
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-08-28 16:08:08 +0000

    Update extras.py

--- userbot/plugins/extras.py
+++ userbot/plugins/extras.py
@@ -3,23 +3,15 @@ import os
 import sys
 
 
-@borg.on(events.NewMessage(pattern=r"^.yo$", outgoing=True))
+@register(events.NewMessage(pattern=r"^.yo$", outgoing=True))
 async def yo(e):
     t = "yo"
     for j in range(15):
         t = t[:-1] + "oo"
-        await e.edit(t)	
-
-
-@borg.on(events.NewMessage(pattern=r"^.oof$", outgoing=True))
-async def yo(e):
-    t = "Oof"
-    for j in range(15):
-        t = t[:-1] + "of"
         await e.edit(t)		      
 
 
-@borg.on(events.NewMessage(pattern=r"^.noice$", outgoing=True))
+@register(events.NewMessage(pattern=r"^.noice$", outgoing=True))
 async def noice(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         if e.fwd_from:
@@ -34,13 +26,13 @@ async def noice(e):
         await e.delete()		      
 
 
-@borg.on(events.NewMessage(pattern=r"^.lol$", outgoing=True))
+@register(events.NewMessage(pattern=r"^.lol$", outgoing=True))
 async def lol(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("L😂L")
 
 
-@borg.on(events.NewMessage(pattern=r"^.alol$", outgoing=True))	
+@register(events.NewMessage(pattern=r"^.alol$", outgoing=True))	
 async def alol(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         t = "L😂L"
@@ -49,7 +41,7 @@ async def alol(e):
             await e.edit(t)
 		
 
-@borg.on(events.NewMessage(pattern=r"^.nice$", outgoing=True))
+@register(events.NewMessage(pattern=r"^.nice$", outgoing=True))
 async def nice(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("Noice")
